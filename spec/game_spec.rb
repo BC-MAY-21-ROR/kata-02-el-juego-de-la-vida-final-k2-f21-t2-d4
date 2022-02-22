595b41fe41ff3457a83c23f9c30ad070e6688e77
Frcomparan <frcomparan@gmail.com>
2022-02-21 19:18:58 -0600

    Add TDD of Game Class
    One TDD added to check the creation of the next_generation method
    
    Co-authored-by: AlejandroCuriel <alex_curielg@hotmail.com>
    Co-authored-by: brocha05 <brocha_05@hotmail.com>
    Co-authored-by: nallely64 <nallelygar.64@gmail.com>

---
+++ spec/game_spec.rb
@@ -0,0 +1,35 @@
+require 'rspec'
+require_relative '../cell.rb'
+require_relative '../grid.rb'
+require_relative '../neighbors.rb'
+require_relative '../game.rb'
+
+describe Game do
+  grid = Grid.new(4,8)
+  # [[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 1, 0, 0, 0],[0, 0, 0, 1, 1, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0]]
+  # Set all the cells with 0
+  grid.cells.size.times do |x|
+    grid.cells[0].size.times do |y|
+      grid.cells[x][y].status = 0
+    end
+  end
+  grid.cells[1][4].status = 1
+  grid.cells[2][3].status = 1
+  grid.cells[2][4].status = 1
+  neighbors = Neighbors.new(4,8,grid.cells)
+  game = Game.new()
+  game.base_grid = grid
+  game.neighbors_grid = neighbors
+  
+  it 'check ' do
+    game.next_generation
+    second_gen = []
+    grid.cells.size.times do |x|
+      second_gen.push([])
+      grid.cells[0].size.times do |y|
+        second_gen[x].push(grid.cells[x][y].status)
+      end
+    end
+    expect(second_gen).to eq ([[0, 0, 0,0,0,0,0,0],[0, 0, 0,1,1,0,0,0],[0, 0, 0,1,1,0,0,0],[0, 0, 0,0,0,0,0,0]])
+  end  
+end
